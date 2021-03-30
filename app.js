579f373593d09fad667f18e6e0a995eeb06797bc
kimlintu <kimlintu@hotmail.com>
2021-03-30 16:38:04 +0200

    basic server code

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 /* SERVER */
 
-    
+const express = require('express');
+const app = express();
+const path = require('path');
+const cfenv = require('cfenv') // Cloud Foundry environment (port, ip etc.)
+
+const servePath = path.join(__dirname, 'build');
+
+app.use(express.static(servePath));
+
+app.get('/', (request, response) => {
+    response.sendFile(path.join(servePath, 'index.html'));
+});
+
+/* Environment for Cloud Foundry app (watchyoursteps). Contains things such as 
+   application port, connected services etc. */
+const app_env = cfenv.getAppEnv({ vcapFile: 'vcap-local.json' });
+
+app.listen(app_env.port || 4096, () => {})
