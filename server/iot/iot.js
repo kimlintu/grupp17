75bdb972a15c31c00e2b19db66c3cb2721d67d5e
kimlintu <kimlintu@hotmail.com>
2021-04-26 22:00:24 +0200

    initial file header info

---
+++ server/iot/iot.js
@@ -0,0 +1,8 @@
+/********************************************************************************
+ * 
+ * This is the API for the IBM Watson IoT service. It provides functionality for:
+ * - Registering, updating, and deleting IoT devices.
+ * - Retrieving registered device information and data.
+ * 
+ ********************************************************************************/
+
