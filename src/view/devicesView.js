2296bd75f7901308639950113d7505a5279adfd3
kimlintu <kimlintu@hotmail.com>
2021-04-28 11:03:44 +0200

    Device name sent to presenter when user clicks 'add device'

--- src/view/devicesView.js
+++ src/view/devicesView.js
@@ -1,23 +1,27 @@
 import { Button, Grid, Paper, TextField } from '@material-ui/core'
+import { useState } from 'react';
 
-const DevicesView = () => {
+const DevicesView = ({ addDevice }) => {
+  const [deviceName, setDeviceName] = useState("device_name");
+
   return <Grid container style={{ "height": "calc(100vh - 65px)", "alignContent": "center" }}>
     <Grid item xs={12}>
       <Grid container justify="center">
         <Paper style={{ "height": 500, "width": 500 }}>
-          <Grid container style={{"padding": "40px"}}>
+          <Grid container style={{ "padding": "40px" }}>
             <Grid item xs={12}>
-              <Grid container justify="center" style={{"padding": "40px"}}>
+              <Grid container justify="center" style={{ "padding": "40px" }}>
                 <form>
-                  <TextField id="outlined-basic" label="Device name" variant="outlined" />
+                  <TextField id="outlined-basic" label="Device name" onChange={(e) => setDeviceName(e.target.value)} variant="outlined" />
                 </form>
               </Grid>
             </Grid>
             <Grid item xs={12}>
               <Grid container justify="center">
-                <Button variant="contained" color="primary" disableElevation style={{"padding": "40px", "fontWeight": "bold"}}>
+                <Button variant="contained" color="primary" disableElevation style={{ "padding": "40px", "fontWeight": "bold" }}
+                        onClick={() => addDevice(deviceName)}>
                   Add device
-              </Button>
+                </Button>
               </Grid>
             </Grid>
           </Grid>
