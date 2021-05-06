92be46061649823d5174de69b05b3006d1a672cd
kimlintu <kimlintu@hotmail.com>
2021-05-06 11:36:00 +0200

    verify correct token length

--- src/presenter/devices/devices.js
+++ src/presenter/devices/devices.js
@@ -9,16 +9,20 @@ function Devices() {
   return createElement(DevicesView, {
     addDevice: async (deviceName, token) => {
       if (deviceName !== "") {
-        try {
-          setStatus({ status: 'loading', message: 'Processing', color: 'primary' });
+        if (token && (token.length <= 8 || token.length >= 36)) {
+          setStatus({ status: 'error', message: 'Token has to be between 8 and 36 characters', color: 'error' });
+        } else {
+          try {
+            setStatus({ status: 'loading', message: 'Processing', color: 'primary' });
 
-          const device = { "deviceName": deviceName, "deviceToken": token };
+            const device = { "deviceName": deviceName, "deviceToken": token };
 
-          const deviceToken = await addDeviceToHub(device);
+            const deviceToken = await addDeviceToHub(device);
 
-          setStatus({ status: 'done', message: 'Device added', parameters: [deviceToken], color: 'primary' });
-        } catch (error) {
-          setStatus({ status: 'error', message: error, color: 'error' });
+            setStatus({ status: 'done', message: 'Device added', parameters: [deviceToken], color: 'primary' });
+          } catch (error) {
+            setStatus({ status: 'error', message: error, color: 'error' });
+          }
         }
       } else {
         setStatus({ status: 'error', message: 'Name cannot be empty', color: 'error' });
