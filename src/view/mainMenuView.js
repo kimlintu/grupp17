237d480ff0cc8b6beefbb044d7537a6fc75e7bdf
nikita <nikita@kth.se>
2021-05-10 16:54:16 +0200

    create new doc in db if current user doesnt have any. Write to database for current user, removed hardcoded user

--- src/view/mainMenuView.js
+++ src/view/mainMenuView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-
+import {apiGetRequest} from '../api/serverApi';
 const MainMenuView = () => {
   return <div>
     <Typography paragraph>
@@ -11,6 +11,13 @@ const MainMenuView = () => {
     <Typography paragraph>
       KIMPOSSIBLE KIMPOSSIBLE KIMPOSSIBLE KIMPOSSIBLE KIMPOSSIBLE KIMPOSSIBLE KIMPOSSIBLE
     </Typography>
+    <button onClick={async () => {
+      const resp = await apiGetRequest({resource: 'api/user'});
+      const data = await resp.json();
+      console.log(data);
+    }}>
+      test
+    </button>
   </div>
 };
 
