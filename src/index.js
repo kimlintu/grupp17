fd031943e2a7b2bd69cf71819ff98fd2c1dedb6d
Daniel Forsling <dforsli@kth.se>
2021-05-11 13:43:55 +0200

    added react context

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ModelContext } from './context';
+import { Model } from './model/model';
+
+const model = new Model();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ModelContext.Provider value={model}>
+      <App />
+    </ModelContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
