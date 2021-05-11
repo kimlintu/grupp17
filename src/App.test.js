ae0d749e7ccf5d2de5f70368c7c5bdabfcdfe1ac
Daniel Forsling <dforsli@kth.se>
2021-05-11 21:04:13 +0200

    changed test to usless

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Simple testing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const a = 10;
+  const b = 2;
+  expect(b).toBeLessThan(a);
 });
