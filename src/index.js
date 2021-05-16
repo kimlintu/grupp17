56c8225f913daa5c530abdb121ef1df0c39269dc
Daniel Forsling <dforsli@kth.se>
2021-05-16 20:05:39 +0200

    added device to context

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { ModelContext } from './context';
+import { ModelContext, DeviceContext } from './context';
 import { Model } from './model/model';
+import Device from './model/device';
 
 const model = new Model();
+const stepcounter = new Device();
 
 ReactDOM.render(
   <React.StrictMode>
     <ModelContext.Provider value={model}>
-      <App />
+      <DeviceContext.Provider value={stepcounter}>
+        <App />
+      </DeviceContext.Provider>
     </ModelContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
