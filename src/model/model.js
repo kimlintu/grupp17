77989eac4b26980d732d48eb1775386710720035
Daniel Forsling <dforsli@kth.se>
2021-05-16 20:07:57 +0200

    improved connection and error handling. and added some comments

--- src/model/model.js
+++ src/model/model.js
@@ -5,95 +5,87 @@ class Model {
         this.id = null;
         this.token = null;
         this.steps = 0;
-        this.active_device = null;
+        this.active_device = new IoTDevice();
         this.subscribers = [];
         this.device_is_connected = false;
     }
 
+    /* Sets the parameters for the stepcounter */
     setParameters(id, token) {
         this.setID(id);
         this.setToken(token);
     }
 
+    /* Sets the id of the device */
     setID(value) {
         this.id = value;
-        console.log("id from model: " + this.id)
     }
 
+    /* Sets the steps-value that will be uploaded */
     setSteps(value) {
         this.steps = value;
     }
 
+    /* Sets the token of the device */
     setToken(value) {
         //    this.notifyObservers();
         this.token = value;
-        console.log("token from model: " + this.token)
     }
 
     getToken() {
         return this.token;
     }
 
+    /* Returns the current date and time as an array */
     getCurrentDateAndTime() {
         let date = new Date();
         let time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
         return [date.toISOString().slice(0, 10), time];
     }
 
+    /* Checks if id and token are not null */
     parametersIsSet() {
         return (this.id !== null && this.token !== null);
     }
 
+    /* Connects device to the IBM-watson-plattform */
     setUpConnection() {
-
         if (this.parametersIsSet()) {
             try {
-                this.active_device = new IoTDevice(this.token, this.id);
-                this.active_device.betaConnection();
+                this.active_device.SetupDevice(this.token, this.id);
+                //this.active_device.betaConnection();
+                // this.device_is_connected = this.is_connected();
+                console.log("mod checks dev-status: " + this.device_is_connected);
 
-                this.device_is_connected = true;
-                console.log("from dev: " + this.active_device.isConnected());
+                this.active_device.on('errormessage', () => {
+                    this.device_is_connected = false;
+                    this.notifyObservers();
+                })
+
+                this.active_device.on('connect', () => {
+                    this.device_is_connected = true;
+                    this.notifyObservers();
+                })
+
+                this.active_device('disconnect', () => {
+                    this.device_is_connected = false;
+                    this.notifyObservers();
+                })
             } catch (er) {
                 throw "Could not connect" + er;
             }
         } else {
             throw "Parameters not set correctly";
         }
-
-    }
-
-    betaUpload() {
-        console.log("model: " + this.token);
-        const [date, time] = this.getCurrentDateAndTime();
-        try {
-            this.active_device.Push('Step-data', { date: date, time_sent: time, token: this.token });
-        } catch (e) {
-            console.log("error:\n");
-            console.log(e);
-        }
-    }
-
-    connectDevice() {
-        console.log("id: " + this.token);
-        /* Edit these lines to reflect your IoT platform config. */
-        // const ACCESS_TOKEN = "eJ&RNc0ALb+Z4(4Vzq";
-        const ACCESS_TOKEN = this.token;
-        try{
-            this.active_device = new IoTDevice(ACCESS_TOKEN, this.id);
-        } catch(err) {
-            throw "Error when connecting. Check config-values";
-        }
-
-        // this.device_is_connected = true;
     }
 
     uploadData() {
-        if (this.active_device.isConnected() === true) {
+        console.log("upload check status: " + this.device_is_connected);   
+        if (this.device_is_connected === true) {
             console.log("model: " + this.token);
             const [date, time] = this.getCurrentDateAndTime();
             try {
                 this.active_device.Push('Step-data', { date: date, time_sent: time, steps: this.steps });
-                //  this.disconnect();
             } catch (e) {
                 console.log("error:\n");
                 console.log(e);
@@ -105,20 +97,20 @@ class Model {
 
     disconnect() {
         this.active_device.Disconnect();
-        this.active_device = null;
+        this.active_device.TeardownDevice();
         //  this.device_is_connected = false;
     }
 
     is_connected() {
-        console.log()
+       // return this.device_is_connected;
         if (this.active_device === null) {
             console.log("null device -> false");
             return false;
         }
         else {
-            console.log("from device: " + this.active_device.isConnected());
+            console.log("model from device: " + this.active_device.isConnected());
             return this.active_device.isConnected();
-        }
+        } 
     }
 
     addObserver(callback) {
