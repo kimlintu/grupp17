9891cf1c34d40ea57f8e764c613961483d51e744
Daniel Forsling <dforsli@kth.se>
2021-05-16 20:05:19 +0200

    added device to context

--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import { createContext } from 'react';
 
 const ModelContext = createContext();
+const DeviceContext = createContext();
 
-export { ModelContext };
+export { ModelContext, DeviceContext };
