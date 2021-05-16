ebe39b0477acaef39024abea1ff7696d7e1c13fe
Daniel Forsling <dforsli@kth.se>
2021-05-16 20:06:55 +0200

    changed implementation of device. improved listening

--- src/model/device.js
+++ src/model/device.js
@@ -1,6 +1,54 @@
 var iotf = require('ibmiotf');
-class Device {
-  constructor(token, id) {
+var EventEmitter = require('events');
+
+class Device extends EventEmitter {
+  constructor() {
+    super();
+    this.device = null;
+    this.device_connected = false;
+  }
+
+  Push(id, data) {
+    this.device.publishHTTPS(id, 'json', JSON.stringify(data), 0);
+  }
+
+  betaConnection() {
+    var that = this;
+    console.log('Dev: connection status before connect ' + that.isConnected())
+    try {
+      this.device.connect();
+      
+      console.log('Dev: connection status after connect ' + that.isConnected())
+      this.whenConnected();
+    } catch (error) {
+      throw "conn-err" + error;
+    }
+
+    /* 
+     this.device.on('upload', function(id, data){
+       this.Push(id, data);
+       });
+    */
+  }
+
+  whenConnected(){
+    var that = this;
+    this.device.on('connect', function () {
+      that.emit('connect', null)
+      console.log("yes connected");
+    });
+
+    this.device.on('disconnect', function () {
+      console.log('Disocnnected');
+      that.emit('disconnected', null);
+    });
+
+    this.device.on('error', function () {
+     that.emit('errormessage', 'An error occured');
+    });
+  }
+
+  SetupDevice(token, id) {
     const device_config = {
       "org": "udbne1",
       "domain": "internetofthings.ibmcloud.com",
@@ -12,36 +60,16 @@ class Device {
     };
     try {
       this.device = new iotf.IotfManagedDevice(device_config);
+      this.device_connected = false;
+      this.betaConnection();
     } catch (error) {
       console.log('ERRORR : ', error)
     }
+  }
+
+  TeardownDevice() {
+    this.device = null;
     this.device_connected = false;
-   // this._setup();
-  }
-
-  Push(id, data) {
-    this.device.publishHTTPS(id, 'json', JSON.stringify(data), 0);
-  }
-
-  betaConnection() {
-    try {
-      console.log('before')
-      this.device.connect();
-      console.log('before')
-
-      this.device_connected = true;
-    } catch (error) {
-      throw "conn-err" + error;
-    }
-
-    /*  this.device.on('connect', function(id, data){
-    // this.Push(id, data);
-     });
-     */
-     this.device.on('upload', function(id, data){
-       this.Push(id, data);
-       });
-
   }
 
   Disconnect() {
@@ -53,45 +81,45 @@ class Device {
     }
   }
 
+  
+  // async _setup() {
 
-  async _setup() {
+  //   var that = this;
+  //   /* Setting the log level to debug. By default its 'warn' */
+  //   //this.device.log.setLevel('debug');
 
-    var that = this;
-    /* Setting the log level to debug. By default its 'warn' */
-    //this.device.log.setLevel('debug');
+  //   /* Connect it to Watson IoT! */
+  //   this.device.connect();
 
-    /* Connect it to Watson IoT! */
-    this.device.connect();
-
-    /* When your device has connected, setup listeners and callbacks. */
-    this.device.on('connect', function (parent) {
-      that.device_connected = true;
-      /*    
-            var rc = that.device.manage(3600, true, true);
+  //   /* When your device has connected, setup listeners and callbacks. */
+  //   this.device.on('connect', function (parent) {
+  //     that.device_connected = true;
+  //     /*    
+  //           var rc = that.device.manage(3600, true, true);
             
-            that.device.on('dmAction', function(request){
-              console.log('Action : ' + request.action);
-              that.device.respondDeviceAction(request,
-                                              that.device.RESPONSECODE.FUNCTION_NOT_SUPPORTED, 
-                                              "Function not supported");
-            });
-       */
-      /* If the device disconnects, we do not need to panic. */
-      that.device.on('disconnect', function () {
-        that.device_connected = false;
-        console.log('Disocnnected');
-      });
-    });
+  //           that.device.on('dmAction', function(request){
+  //             console.log('Action : ' + request.action);
+  //             that.device.respondDeviceAction(request,
+  //                                             that.device.RESPONSECODE.FUNCTION_NOT_SUPPORTED, 
+  //                                             "Function not supported");
+  //           });
+  //      */
+  //     /* If the device disconnects, we do not need to panic. */
+  //     that.device.on('disconnect', function () {
+  //       that.device_connected = false;
+  //       console.log('Disocnnected');
+  //     });
+  //   });
 
-    /* Errors are pretty bad, right? */
-    this.device.on('error', function (argument) {
-      try{
-        console.log('errpr: ', argument);
-      } catch(err) {
-        throw "argument";
-      }
-    });
-  }
+  //   /* Errors are pretty bad, right? */
+  //   this.device.on('error', function (argument) {
+  //     try {
+  //       console.log('errpr: ', argument);
+  //     } catch (err) {
+  //       throw "argument";
+  //     }
+  //   });
+  // }
 
   isConnected() {
     return this.device_connected;
