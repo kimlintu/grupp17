538cb13abea268e87efa30091b385d6d5dcb22e3
Zarcez <55078381+Zarcez@users.noreply.github.com>
2021-05-16 21:40:32 +0200

    Revert "Revert "Merge branch 'main' into Carl""
    This reverts commit 63205e9138adb407bdaedd93d701983617216032.

--- src/view/mainMenuView.js
+++ src/view/mainMenuView.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-
-const MainMenuView = () => {
+import {apiGetRequest} from '../api/serverApi';
+const MainMenuView = ({getUserName, userName}) => {
+  getUserName(); //get logged in users name, if there is any
   return <div>
     <Typography paragraph>
       KIMPOSSIBLE
@@ -9,8 +10,22 @@ const MainMenuView = () => {
       KIMPOSSIBLE
     </Typography>
     <Typography paragraph>
-      KIMPOSSIBLE KIMPOSSIBLE KIMPOSSIBLE KIMPOSSIBLE KIMPOSSIBLE KIMPOSSIBLE KIMPOSSIBLE
+      Hello {userName} ! Enjoy your stay...
     </Typography>
+    <button onClick={async () => {
+      const resp = await apiGetRequest({resource: 'api/user'});
+      const data = await resp.json();
+      console.log(data);
+    }}>
+      print autheticationinfo in console
+    </button>
+    <button onClick={async () =>{
+      const resp1 = await apiGetRequest({resource: 'db/delete'});
+      const data = await resp1.json();
+      console.log(data.ok);
+    }}>
+      test db delete
+    </button>
   </div>
 };
 
