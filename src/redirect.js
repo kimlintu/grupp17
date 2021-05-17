9f8708e8ab646ebaad3e0ebccb5c6494d4cc1e5c
kimlintu <kimlintu@hotmail.com>
2021-05-17 22:19:29 +0200

    redirect to '/account' if nto loggd in

---
+++ src/redirect.js
@@ -0,0 +1,41 @@
+import { useEffect, useState, cloneElement } from "react";
+import { createElement } from "react";
+import { getUserInfo } from "./api/serverAuthApi";
+
+function RedirectWrapper(props) {
+  const [checkedUserStatus, setCheckedUserStatus] = useState(false);
+  const [updateState, setUpdateState] = useState(0);
+
+  // Try to retreive user info to check if the user is logged in. 
+  useEffect(() => {
+    console.log('WINODW: ', window.loggedIn)
+
+    if (!checkedUserStatus && !(window.loggedIn === true)) {
+      getUserInfo().then((user) => {
+        if (user.name) {
+          setUpdateState(updateState + 1);
+          window.loggedIn = true;
+        } else {
+          setUpdateState(updateState + 1);
+          window.loggedIn = false;
+        }
+
+        setCheckedUserStatus(true);
+      })
+    }
+  }, [])
+
+
+  if (updateState === 0) {
+    // Display loading status
+    return createElement('div', {}, "LOADING PAGE")
+  } else if ((window.loggedIn === true) || (window.location.pathname === '/account')) {
+    // Give them requested page
+    return createElement('div', {}, props.children)
+  } else {
+    // User not logged in, return account login page.
+    window.location.pathname = '/account'
+  }
+}
+
+export { RedirectWrapper };
