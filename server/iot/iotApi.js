3198d72a056f4b33581cb1c13f7f44aaa55bec31
kimlintu <kimlintu@hotmail.com>
2021-05-18 09:44:16 +0200

    add eventual query parameters to GET request

--- server/iot/iotApi.js
+++ server/iot/iotApi.js
@@ -16,11 +16,21 @@ const iotServiceInfo = {
  * 
  * @returns {object} the response of the request.
  */
-async function iotApiCall({ resource, method, headers, body }) {
+async function iotApiCall({ resource, params, method, headers, body }) {
   if (!resource || !method)
     throw 'Missing parameters: [' + (resource ? '' : ' [resource] ') + (method ? '' : ' [method] ') + ']';
 
-  const fullUrl = iotServiceInfo.url + resource;
+  let fullUrl = iotServiceInfo.url + resource;
+
+  // If there are any query parameters we add them after the resource.
+  if (params) {
+    let paramString = "?";
+    params.forEach(parameter => {
+      paramString += `${parameter.key}=${parameter.value}&`;
+    });
+
+    fullUrl += `/${paramString}`;
+  }
 
   if (!headers)
     headers = {};
@@ -73,8 +83,8 @@ async function iotApiAddDevice({ device }) {
  * 
  * @returns {array} the list of devices as an array.
  */
-async function iotApiGetDeviceList({ user }) {
-  const deviceListData = await iotApiGetResponseData(iotApiCall, { resource: 'bulk/devices', method: 'GET' }, 200);
+async function iotApiGetDeviceList({ deviceId }) {
+  const deviceListData = await iotApiGetResponseData(iotApiCall, { resource: 'bulk/devices', params: [{ key: "deviceId", value:  deviceId }], method: 'GET' }, 200);
 
   return deviceListData;
 }
