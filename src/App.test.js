b137efed51571b0dedbf92b21ab75625a452c09b
Daniel Forsling <dforsli@kth.se>
2021-05-19 17:13:55 +0200

    app.test.js now has a valid test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,5 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText("LOADING PAGE");
-  expect(linkElement).toBeInTheDocument();
+  expect(document.body.textContent).toContain('LOADING PAGE');
 });
