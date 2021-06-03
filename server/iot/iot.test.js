3be7554ed252a7c6453222dbd17f3d067041b6f8
kimlintu <kimlintu@hotmail.com>
2021-06-03 18:39:48 +0200

    the iot tests

--- server/iot/iot.test.js
+++ server/iot/iot.test.js
@@ -6,4 +6,18 @@ test('should return added device token', async () => {
   const deviceInfo = await addDevice({ user: 'test', deviceName: 'test' });
 
   expect(deviceInfo.authToken).toBeDefined();
-}); 
+});
+
+test('missing parameters should fail', async () => {
+  try {
+    await addDevice({ deviceName: 'test' });
+  } catch (e) {
+    expect(e).toEqual('Missing either user or deviceName parameter.');
+  }
+
+  try {
+    await addDevice({ user: 'test' });
+  } catch (e) {
+    expect(e).toEqual('Missing either user or deviceName parameter.');
+  }
+});
